dbaced1aa88fb3802fc02e35df05a55d33eb3c65
Kevin Sommer <kevin@psoma.de>
2022-11-02 07:33:00 +0100

    Initial version

---
+++ FRET.JsApi.Abstraction/Models/JsApiCallErrorCode.cs
@@ -0,0 +1,58 @@
+﻿namespace FRET.JsApi.Abstraction.Models;
+
+/// <summary>
+/// Enumeration of all possible technical error codes used in <see cref="JsApiCallResult"/>.
+/// </summary>
+public enum JsApiCallErrorCode
+{
+    /// <summary>
+    /// No error occurred. 
+    /// Call was successful.
+    /// </summary>
+    None,
+
+    /// <summary>
+    /// Login failed. 
+    /// No specific error defails available.
+    /// </summary>
+    LoginGenericError,
+
+    /// <summary>
+    /// Logout failed.
+    /// There is no logged in user at the moment.
+    /// </summary>
+    LogoutNoUserLoggedIn,
+
+    /// <summary>
+    /// Generic error.
+    /// Provided song id is invalid. Ensure that it's provided in valid GUID format.
+    /// </summary>
+    SongIdInvalidFormat,
+
+    /// <summary>
+    /// Song not found.
+    /// There is no active song with this id in the database.
+    /// </summary>
+    /// <remarks>
+    /// It might become available soon (e.g. if the cache is not loaded after startup).
+    /// </remarks>
+    SongNotFound,
+
+    /// <summary>
+    /// Song not loaded.
+    /// Song could not be loaded from the server or to the player.
+    /// </summary>
+    SongNotLoaded,
+
+    /// <summary>
+    /// Player action not possible.
+    /// Player is in a invalid state. Use force-flag to force this action.
+    /// </summary>
+    PlayerInvalidState,
+
+    /// <summary>
+    /// Player action not possible.
+    /// Player is in an unknown state.
+    /// </summary>
+    PlayerUnknownState,
+}
