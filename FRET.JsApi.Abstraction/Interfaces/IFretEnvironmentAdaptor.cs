8aff822cddf64ea6ddedcf530fb7feb636dd18a6
ITDancer13 <kevin@ksommer.eu>
2022-11-05 10:26:50 +0100

    * Add FRET Hosting Environment Endpoint (#3)
    * Rename Interfaces and Models

---
+++ FRET.JsApi.Abstraction/Interfaces/IFretEnvironmentAdaptor.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using FRET.JsApi.Abstraction.Models;
+
+namespace FRET.JsApi.Abstraction.Interfaces;
+
+/// <summary>
+/// Interface to access information about FRET Hosting Environment.
+/// </summary>
+public interface IJsApiFretEnvironmentWrapper
+{
+    /// <summary>
+    /// Provides information about FRET Hosting Environment.
+    /// </summary>
+    Task<FretEnvironmentInfo> GetFretEnvironmentInfo();
+}
---
+++ FRET.JsApi.Abstraction/Models/FretEnvironmentInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace FRET.JsApi.Abstraction.Models;
+
+/// <summary>
+/// Provides information about FRET Hosting Environment.
+/// </summary>
+public class FretEnvironmentInfo
+{
+    public string FretVersion { get; set; }
+}
